e32bf2e4cd22141dd9e6c8acfca4b15ef7c15c68
Trim21 <trim21me@gmail.com>
2019-03-27 17:08:28 +0800

    add redirect

--- refreshCdn.js
+++ refreshCdn.js
@@ -1,10 +1,10 @@
-const qcloudSDK = require('qcloud-cdn-node-sdk');
+const qcloudSDK = require('qcloud-cdn-node-sdk')
 qcloudSDK.config({
-    secretId: process.env.SECRET_ID,
-    secretKey: process.env.SECRET_KEY
+  secretId: process.env.SECRET_ID,
+  secretKey: process.env.SECRET_KEY
 })
 qcloudSDK.request('RefreshCdnDir', {
-    'dirs.0': 'https://blog.trim21.cn/'
+  'dirs.0': 'https://blog.trim21.cn/'
 }, res => {
-    console.log(res)
+  console.log(res)
 })
