fda8766ad85dcae7dfd196b901f28ac3e42c8ba5
Trim21 <trim21me@gmail.com>
2019-03-27 19:45:52 +0800

    fix

---
+++ scripts/html_minifier.js
@@ -0,0 +1,18 @@
+/* global hexo:true */
+'use strict'
+const minify = require('html-minifier').minify
+const options = {
+  removeComments: true,
+  collapseWhitespace: true,
+  collapseBooleanAttributes: true,
+  removeEmptyAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  ignoreCustomComments: [/^\s*more/]
+}
+
+hexo.extend.filter.register('after_render:html',
+  function (str) {
+    return minify(str, options)
+  }
+)
