82895b60c0ec8a622cddb4c794f52a8289414269
Trim21 <i@trim21.me>
2019-11-01 19:57:59 +0800

    deploy to cos on netlify

--- refreshCdn.js
+++ refreshCdn.js
@@ -1,10 +1,13 @@
-const qcloudSDK = require('qcloud-cdn-node-sdk')
+const qcloudSDK = require('qcloud-cdn-node-sdk');
+
 qcloudSDK.config({
-  secretId: process.env.SECRET_ID,
-  secretKey: process.env.SECRET_KEY
-})
-qcloudSDK.request('RefreshCdnDir', {
-  'dirs.0': 'https://blog.trim21.cn/'
+  secretId: process.env.COS_SECRET_ID,
+  secretKey: process.env.COS_SECRET_KEY
+});
+
+qcloudSDK.request('RefreshCdnUrl', {
+  'urls.0': 'https://blog.trim21.cn/'
 }, res => {
-  console.log(res)
-})
+  console.log(res);
+  console.log('refresh cdn index')
+});
