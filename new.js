24c93e019b46db777c9b30b1bc8399bd314b795a
Trim21 <i@trim21.me>
2019-12-20 03:51:03 +0800

    use utteranc.es as comments

--- new.js
+++ new.js
@@ -1,10 +1,10 @@
-var Hexo = require('hexo');
-var hexo = new Hexo(process.cwd(), {});
+const Hexo = require('hexo');
+const hexo = new Hexo();
 
 hexo.init().then(function () {
-    hexo.post.create({
-        slug: process.argv.slice(2).join('-'),
-        title: process.argv.slice(2).join(' '),
-        tags: null,
-    }, false);
+  hexo.post.create({
+    slug: process.argv.slice(2).join('-'),
+    title: process.argv.slice(2).join(' '),
+    tags: null,
+  }, false);
 });
