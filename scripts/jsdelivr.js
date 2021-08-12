d1a5efd23396a46cd5c9c784905965d8b0e7ff63
Trim21 <trim21.me@gmail.com>
2021-08-13 03:05:51 +0800

    update

--- scripts/jsdelivr.js
+++ scripts/jsdelivr.js
@@ -4,23 +4,22 @@
  * Syntax:
  *   {% fancybox /path/to/image [/path/to/thumbnail] [title] %}
  */
-const { htmlTag } = require('hexo-util');
+const { htmlTag } = require("hexo-util");
 const Url = require("url");
 const { posix } = require("path");
 
-
 hexo.extend.tag.register("jsdelivr", function (args) {
-  let [path, title,] = args;
-  let { source } = this
-  if (path[0] !== '/') {
-    path = posix.join(source, path)
+  let [path, title] = args;
+  let { source } = this;
+  if (path[0] !== "/") {
+    path = posix.join(source, path);
   }
-  let u = new Url.URL(`https://cdn.jsdelivr.net` + posix.join('/gh/trim21/blog@master/source', path))
+  let u = new Url.URL(`https://cdn.jsdelivr.net` + posix.join("/gh/trim21/blog", path));
 
   const attrs = {
     src: u.toString(),
     title,
-  }
+  };
 
-  return htmlTag('img', attrs);
+  return htmlTag("img", attrs);
 });
